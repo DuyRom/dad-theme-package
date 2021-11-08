1f1e575b5f00147a68525142278d4d82ffc6c40e
DuyRom <duyit192@hotmail.com>
2021-11-08 11:43:53 +0700

    remove form input label

--- resources/views/components/file-input.blade.php
+++ resources/views/components/file-input.blade.php
@@ -1,5 +1,4 @@
 <fieldset class="form-group">
-    <label for="basicInputFile">{{$attributes['label'] ?? NULL}}</label>
     <div class="custom-file">
         <input {{$attributes->merge(['class'=>'custom-file-input'])}} type="file">
         <label class="custom-file-label" for="inputGroupFile01">Chọn file</label>
