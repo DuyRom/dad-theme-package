5e68fc8f0d54e885a7ec46869c6b72c8c59b804b
DuyRom <duyit192@hotmail.com>
2021-11-09 16:41:56 +0700

    fix horizontal layout component

--- resources/views/components/hol-layout.php
+++ resources/views/components/hol-layout.php
@@ -1,3 +1,5 @@
 <section id="basic-horizontal-layouts">
-	 <div class="row match-height">{{$layoutContent ?? NULL}}</div>
+	<div class="row match-height">
+	 {{$layoutContent ?? NULL}}
+	</div>
 </section>
