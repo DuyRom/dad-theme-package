4c05661a387c3131a2393825a23896a3474b564b
DuyRom <duyit192@hotmail.com>
2021-11-10 16:31:16 +0700

    fix feather font src

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -20,7 +20,7 @@
                     </li>
                     {{$notifyHeader ?? NULL}}
                     <li class="dropdown dropdown-user nav-item"><a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">
-                            <div class="user-nav d-sm-flex d-none"><span class="user-name text-bold-600">{{$attributes['user_name']}}</span><span class="user-status">{{$attributes['room']}}</span></div><span><img class="round" src="/vendor/dadtheme/images/avatar-s-11.jpg" alt="avatar" height="40" width="40"></span>
+                            <div class="user-nav d-sm-flex d-none"><span class="user-name text-bold-600">{{$attributes['user_name']}}</span><span class="user-status">{{$attributes['room']}}</span></div><span><img class="round" src="/duynguyen/dadtheme/images/avatar-s-11.jpg" alt="avatar" height="40" width="40"></span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right">
                             {{$drnUser ?? NULL}}
